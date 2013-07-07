e515c424cb7e2a5c66de5ae7623f18dbe879a98a
markusfh <markusfh@gmail.com>
2013-07-07 15:00:46 +0000

    final

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 // read file
 var fs = require('fs');
-var data = fs.readFileSync("./index.html");
+var data = fs.readFileSync("./index.html") || "No index.html found";
 
 app.get('/', function(request, response) {
   response.send(data);
